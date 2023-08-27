d7faa784e738cdbc095d478098c125b7710574e0
Martin Carolan <martin@typesystems.co.uk>
2023-08-27 15:56:48 -0500

    Hmm

--- ts/nav.tsx
+++ ts/nav.tsx
@@ -1,6 +1,7 @@
 function hideNav() {
     const navCheck = document.getElementById("nav-check") as HTMLInputElement;
     if (navCheck) {
+        navCheck.removeAttribute("checked");
         navCheck.checked = false;
     }
     return true;
